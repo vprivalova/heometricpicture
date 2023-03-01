53fd9314a76c7e62fcab6c1899bb070ce84c4f4b
vprivalova <privalovaa.vv@gmail.com>
2023-03-01 14:13:59 +0700

    Functions and parameteres are described

--- main.py
+++ main.py
@@ -30,56 +30,89 @@ def background():
 background()
 
 
-def triangle():
+def triangle(x, y, a, c, angle1, fillcolour, pensize, pencolor):
     """
     TODO: Ksenia
-    Function is drawing a triangle.
+    Function is drawing an isosceles triangle. #равнобедренный треугольник
+    :param x: first coordinate of the start of drawing triangle
+    :param y: last coordinate of the start of drawing triangle
+    :param a: lenght of the side of the triangle, other side (b) is equal
+    :param c: length of the base of the triangle
+    :param angle1: an angle between sides a and b. other two angles (angle2, angle3) are same and equal = (180-angle1)/2 #угол между равными сторонами
+    :param fillcolour: triangle fill colour #цвет заливки треугольника
+    :param pensize: contour width
+    :param pencolor: colour of the contour
     :return:
     """
     pass
 
 
-def star():
+def star(x, y, a, fillcolour, pensize, pencolour):
     """
     TODO: Nastya
     Function is drawing a star.
+    :param x: first coordinate of the start of drawing star
+    :param y: second coordinate of the start of drawing star
+    :param a: length of the side of star
+    :param fillcolour: star fill colour
+    :param pensize: contour width
+    :param pencolour: colour of the contour
     :return:
     """
-    t.pendown()
-    t. speed(0)
+    t.pd()
+    t.speed(0)
     t.pensize(2)
     t.color('pink')
     for i in range(250):
         for colours in ['red', 'blue']:
             t.color(colours)
-            t.forward(2*i)
-            t.left(150)
-    t.forward(500)
-    t.exitonclick()
+            t.fd(2*i)
+            t.lt(150)
+    t.fd(500)
+    t.done()
 
 
-def circle():
+def circle(x, y, radius, fillcolour, pensize, pencolour):
     """
-    TODO:
+    TODO: Vika
     Function is drawing a circle.
+    :param x: first coordinate of the center of the circle
+    :param y: second coordinate of the center of the circle
+    :param radius: half of diameter of the circle
+    :param fillcolour: circle fill colour
+    :param pensize: contour width
+    :param pencolour: colour of the contour
     :return:
     """
     pass
 
 
-def square():
+def square(x, y, a, fillcolour, pensize, pencolour):
     """
-    TODO:
+    TODO: Nastya
     Function is drawing a square.
+    :param x: first coordinate of the start of drawing square
+    :param y: second coordinate of the start of drawing square
+    :param a: length of the side of the square
+    :param fillcolour: square fill colour
+    :param pensize: contour width
+    :param pencolour: colour of the contour
     :return:
     """
     pass
 
 
-def rectangle():
+def rectangle(x, y, a, b, fillcolour, pensize, pencolour):
     """
-    TODO:
-    Function is drawing rectangle.
+    TODO: Ksenia
+    Function is drawing a rectangle.
+    :param x: first coordinate of the start of drawing rectangle
+    :param y: second coordinate of the start of drawing rectangle
+    :param a: length of the longer side
+    :param b: length of the shorter side
+    :param fillcolour: rectangle fill colour
+    :param pensize: contour width
+    :param pencolour: colour of the contour
     :return:
     """
     pass
